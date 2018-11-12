69ef88eb04850a8b58810ebe88021d682e9fe653
v123582 <v123582@gmail.com>
2018-11-12 14:39:07 +0800

    attach first view to express

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
 const Url = require('./models/url')
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 
 const mongoURI = 'mongodb://localhost/l3-express-url-shortener'
 mongoose.connect(mongoURI, { useNewUrlParser: true }, (err, db) => {
@@ -11,7 +15,9 @@ mongoose.connect(mongoURI, { useNewUrlParser: true }, (err, db) => {
   console.log(`Connected to MongoDB`)
 })
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`)
